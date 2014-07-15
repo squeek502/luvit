44c83684111a5e9678a16738cd3ff8ea13481e5f
Ryan Phillips <ryan.phillips@rackspace.com>
2014-07-14 20:33:18 -0500

    remove the loop

--- lib/luvit/utils.lua
+++ lib/luvit/utils.lua
@@ -184,9 +184,6 @@ function utils.dump(o, depth, no_colorize, seen_tables)
     end
     if estimated > 200 then
       local s = "{\n  " .. indent
-      for k, v in pairs(lines) do
-        s = s .. v .. ",\n  " .. indent
-      end
       s = s .. "\n" .. indent .. "}"
       return s
     else
